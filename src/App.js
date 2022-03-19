6449f736d92b757747d78173d7c0884ec7c43167
PuneetP16 <patelpunit95@gmail.com>
2022-03-19 12:30:45 +0530

    add: product and categories data

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import "./App.css";
 import { MockBee } from "./backend/mockdocs/MockBee";
 import { Routes, Route, Link } from "react-router-dom";
 import MockAPI from "./backend/mockdocs/MockMan";
+import { Home } from "./pages";
 
 function App() {
 	return (
 		<div className="App">
 			<Routes>
+				<Route path="/" element={<Home />} />
 				<Route path="/mockbee" element={<MockBee />} />
 				<Route path="/mockman" element={<MockAPI />} />
 			</Routes>
-
-			<nav>
-				<Link to="/mockbee">MockBee</Link>
-				<Link to="/mockman">Mockman</Link>
-			</nav>
 		</div>
 	);
 }
